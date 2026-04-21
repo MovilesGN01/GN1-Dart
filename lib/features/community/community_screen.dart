781e52c8e4192e50093fe45c47ca1e1f8071b8c9
32DANIEL <d.rudas@uniandes.edu.co>
2026-04-21 02:34:48 -0500

    Funcional

--- lib/features/community/community_screen.dart
+++ lib/features/community/community_screen.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:uniride/shared/widgets/bottom_nav_bar.dart';
 
 class CommunityScreen extends StatelessWidget {
   const CommunityScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Community',
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.w700,
-        ),
+    return const Scaffold(
+      body: Center(
+        child: Text('Community'),
       ),
+      bottomNavigationBar: UniRideBottomNav(currentIndex: 2),
     );
   }
 }
